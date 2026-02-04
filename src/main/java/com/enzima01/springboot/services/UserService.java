ce0c28765469515150c474b8ba564fa7a0c205d6
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-04 19:36:20 -0300

    Service layer, component registration

---
+++ src/main/java/com/enzima01/springboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.enzima01.springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enzima01.springboot.entities.User;
+import com.enzima01.springboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
