12c05bf35678029007a40b7b294c902a907a5682
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-04 18:32:43 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/enzima01/springboot/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.enzima01.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enzima01.springboot.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
