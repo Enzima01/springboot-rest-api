ca45703251f8e94105e0fb659b947bf517366acd
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-05 20:14:22 -0300

    Subtotal & Total methods

--- src/main/java/com/enzima01/springboot/entities/Order.java
+++ src/main/java/com/enzima01/springboot/entities/Order.java
@@ -39,10 +39,9 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 
-	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
@@ -90,11 +89,6 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-	
 	public Payment getPayment() {
 		return payment;
 	}
@@ -105,6 +99,19 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	@Override
